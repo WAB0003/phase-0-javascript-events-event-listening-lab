b99e665ea0a64e89cbbe2e68db90e6ffc0d08734
Bill Brown <bill.a.brown90@gmail.com>
2023-02-24 08:48:18 -0800

    add a Click Alert Event

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
+    const input = document.querySelector("#button");
+    input.addEventListener('click', clickAlert);
+    
+    function clickAlert () {
+        alert("I was clicked!");
+    }
 }
+
+
